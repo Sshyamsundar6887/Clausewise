b991a26c98add0f0ac8538ec2b67b1f208b88351
bharathwajamahendar05 <bharathwajmahendar@gmail.com>
2026-08-17 12:38:44 +0530

    New

--- clausewise_implementation.py
+++ clausewise_implementation.py
@@ -815,7 +815,7 @@ def format_clauses(results: Dict[str, Any]) -> str:
                     f'</div>'
                     f'<div class="clause-text">{clause.get("text", "")}</div>'
                     f'</div>'
-                )
+                )###
             
             parts.extend(['</div>', '</div>'])
     
